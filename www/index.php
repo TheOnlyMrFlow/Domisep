eb06edde342a046ae98286bce55323d2b08a696e
intern@cleverrobotlabs.com <comte.florian@gmail.com>
2018-11-05 16:59:16 +0100

    joint footer et homepage

---
+++ www/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset="utf-8" />
+  <title>Domisep</title>
+  
+  <link rel="stylesheet" type="text/css" media="screen" href="../style/homepage.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="../style/modal.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="../style/style.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="../style/footer.css" />
+
+<body>
+  <?php include('html/homepage.html'); ?>
+	<?php include('html/footer.html'); ?>
+</body>
+
+</html>
