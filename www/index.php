7362e10b7b024f75c0eb27a60d4d33be1c336ee5
intern@cleverrobotlabs.com <comte.florian@gmail.com>
2018-11-06 15:18:57 +0100

    c'est clean sa daronne mais encore plus

--- www/index.php
+++ www/index.php
@@ -7,10 +7,10 @@
 	<title>Domisep</title>
 
 	<link rel="stylesheet" type="text/css" media="screen" href="../style/style.css" />
-	<link rel="stylesheet" type="text/css" media="screen" href="components/modals/modal.css"/>
+	<link rel="stylesheet" type="text/css" media="screen" href="components/modals/modal.css" />
 	<link rel="stylesheet" type="text/css" media="screen" href="components/header-nav/header-nav.css" />
 
-	
+
 
 </head>
 
@@ -19,9 +19,9 @@
 
 	include('components/header-nav/header-nav.html');
 
-	  include('pages/homepage/homepage.html');
+	include('pages/homepage/homepage.html');
 	  
-	  include('components/footer/footer.php'); 
+	include('components/footer/footer.php'); 
 
 	if (isset($_GET['contact'])){
 		include('components/modals/contact/contact.php');
