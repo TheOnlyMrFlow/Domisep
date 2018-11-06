d84f4d8f159e904a7ab0e11c106e2305916ea978
intern@cleverrobotlabs.com <comte.florian@gmail.com>
2018-11-06 10:57:53 +0100

    integration des petites modifs qualis d'alex

---
+++ www/components/footer.php
@@ -0,0 +1,64 @@
+<div class="footer">
+
+
+	<!-- Trigger/Open The Modal -->
+	<p>&copy; Copyright 2018 Domisep. All rights reserved.</p>
+	<button id="button-contact">Contact us</button>
+
+	<!-- The Modal -->
+
+
+</div>
+
+
+
+<div id="modal-contact" class="modal">
+
+	<?php
+//if "email" variable is filled out, send email
+  if (isset($_REQUEST['request-email']))  {
+
+    $to      = "comte.florian@gmail.com";
+    $subject = $_REQUEST['request-subject'];
+    $message = $_REQUEST['request-content'];
+    $headers = array(
+        'From' => 'comte.florian@gmail.com',
+        'Reply-To' => 'comte.florian@gmail.com',
+        'X-Mailer' => 'PHP/' . phpversion()
+    );
+    
+    mail($to, $subject, $message, $headers);
+    
+   echo '<script>console.log("Sent")</script>';
+  }
+  
+  //if "email" variable is not filled out, display the form
+  else  {
+    echo '<script>console.log("Not sent")</script>';
+?>
+
+	<!-- Modal content -->
+	<div class="modal-content">
+		<span class="close" id="close-contact">&times;</span>
+		<form action="/" method="post">
+
+			<h2>Contact us</h2>
+
+			<input type="text" name="request-email" placeholder="Your e-mail adress" />
+			<br>
+			<input type="text" name="request-subject" placeholder="Object of your request" />
+			<br>
+			<textarea style="resize: none;" cols="86" rows="20" name="request-content"></textarea>
+			<br>
+			<input type="submit" value="Send">
+		</form>
+	</div>
+
+</div>
+
+<script src="../scripts/modal-contact.js">
+</script>
+
+<?php
+ } 
+?>
--- www/index.php
+++ www/index.php
@@ -4,16 +4,18 @@
 
 <head>
 	<meta charset="utf-8" />
-  <title>Domisep</title>
-  
-  <link rel="stylesheet" type="text/css" media="screen" href="../style/homepage.css" />
-  <link rel="stylesheet" type="text/css" media="screen" href="../style/modal.css" />
-  <link rel="stylesheet" type="text/css" media="screen" href="../style/style.css" />
-  <link rel="stylesheet" type="text/css" media="screen" href="../style/footer.css" />
+	<title>Domisep</title>
 
-<body>
-  <?php include('html/homepage.html'); ?>
-	<?php include('html/footer.html'); ?>
-</body>
+	<link rel="stylesheet" type="text/css" media="screen" href="../style/homepage.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="../style/modal.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="../style/style.css" />
+	<link rel="stylesheet" type="text/css" media="screen" href="../style/footer.css" />
+
+	<body>
+	<?php 
+  include('components/homepage.html');
+  include('components/footer.php'); 
+  ?>
+	</body>
 
 </html>
