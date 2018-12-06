5052fe98e6483bd7c99af49e2b0ef962adff1136
alex <alex.de-coat@orange.fr>
2018-12-06 15:48:27 +0100

    nouvelle arbo

--- www/newpreset2.php
+++ www/newpreset2.php
@@ -12,7 +12,7 @@ require("scripts/fonction_php_component.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Create Preset</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="createpreset2.css" />
+    <link rel="stylesheet" type="text/css" href="./style/createpreset2.css" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
         crossorigin="anonymous">
       <link rel="stylesheet" type="text/css" media="screen" href="../style/dashboard-style.min.css" />
@@ -80,6 +80,6 @@ include 'components/header-nav/header-nav.php';
 
 </body>
 
-<script src="./createpreset2.js"></script>
+<script src="./scripts/createpreset2.js"></script>
 
 </html>
