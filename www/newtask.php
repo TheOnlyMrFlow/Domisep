a00668edb38be7e559c26e9dd1180c26e81bb3d7
alex <alex.de-coat@orange.fr>
2018-12-10 16:20:19 +0100

    new css/html for newtask

--- www/newtask.php
+++ www/newtask.php
@@ -15,7 +15,9 @@
     <link rel="stylesheet" href="components/header-nav/header-dashboard.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="components/header-nav/sticky-header.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="../style/newtask.css"/>
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css"/>
+    <link rel="stylesheet" type="text/css" href="style/newtask.css"/>
+    <script src="main.js"></script>
 </head>
 <body>
   <?php
@@ -29,71 +31,64 @@ include 'components/header-nav/header-nav.php';
 
 ?>
 <div class="page-content-container">
-  <div class="page-content">
-    <div id="NewTask">
-        <div id="contentNewTask">
-                <h1 id="titleNewTask">New task</h1>
-                <div id="c1">
-                    Component/preset
-                    <!-- <form action="/action_page.php"> -->
-                    <br/>Frequency
-                    <br/>Start Date
-                </div>
-                <div id="c2">
-                        <div id="component">
-                            <form>
-                                <select name="components">
-                                  <option value="Light A">Lampe A</option>
-                                  <option value="Heat A">Chauffage A</option>
-                                  <option value="Light B">Lampe B</option>
-                                  <option value="Stores">Volets A</option>
-                                </select>
-                                <input type="submit" value="Submit">
-                            </form></div><br/>
-                        <div id="frequency">
-                            <form>
-                                <select name="frequency">
-                                    <option value="Daily">Daily</option>
-                                    <option value="Hourly">Hourly</option>
-                                    <option value="Weekly">Weekly</option>
-                                </select>
-                                <input type="submit" value="Submit">
-                            </form>
-                        </div><br/>
-                        <div id="time">
-                            <form>
-                                <input type="datetime-local" id="meeting-time"
-                                name="meeting-time" value="2018-06-12T19:30"
-                                min="2018-06-07T00:00" max="2018-06-14T00:00">
-                            </form>
-                        </div><br/>
+    <div class="page-content">
+        <div id="NewTask">
 
-                </div>
-                <div id="c3">State</div>
-                <div id="c4">
-                    <form>
-                        <select name="State">
+            <div id="title">
+                <h1 id="titleNewTask">New task</h1>
+            </div>
+
+            <div class="total-wrapper">
+
+                <div class="half-wrapper">
+                    <section class = "selectors">
+                        <span class ="label">Component</span>
+                        <select class ="dropdown" name="components">
+                            <option value="notanoption" disabled selected>-- select a component --</option>
+                            <option value="Light A">Lampe A</option>
+                            <option value="Heat A">Chauffage A</option>
+                            <option value="Light B">Lampe B</option>
+                            <option value="Stores">Volets A</option>
+                        </select>
+                    </section>
+                    <section class = "selectors">
+                        <span class="label">State</span>
+                        <select class ="dropdown" name="State">
+                            <option value="notanoption" disabled selected>-- select a state --</option>
                             <option value="ON">ON</option>
                             <option value="OFF">OFF</option>
                         </select>
-                        <input type="submit" value="Submit">
-                    </form></div>
-                <div id="c5">Value</div>
-                <div id="c6">
-                    <form>
-                        <select name="Value">
-                            <option value="tbd">to be disclosed</option>
+                    </section>
+                    <section class = "selectors">
+                        <span class="label">Value</span>
+                        <select class ="dropdown" name="Value">
+                            <option value="tbd" disabled selected>-- to be disclosed --</option>
                         </select>
-                        <input type="submit" value="Submit">
-                    </form>
-                </div>
-        </div>
-        <div id="SaveTask">
-        <input id="buttonSaveTask" type="button" value="Save the task">
-        </div>
-    </div>
+                    </section>
+                    <section class = "selectors">
+                        <span class ="label">Frequency</span>
+                        <select class ="dropdown" name="frequency">
+                            <option value="notanoption" disabled selected>-- select frequency --</option>
+                            <option value="Daily">Daily</option>
+                            <option value="Hourly">Hourly</option>
+                            <option value="Weekly">Weekly</option>
+                         </select>
+                    </section>
+                    <section class = "selectors">
+                        <span class="label">Date</span>
+                        <input type="date" id="start" name="trip-start"
+                                value="2018-07-22"
+                                min="2018-01-01" max="2018-12-31">
+                    </section>
+                 </div>
 
-
+                 <div class="half-wrapper">
+                     <section class = "saveSection">
+                     <input id="buttonSaveTask" type="button" value="Save the task">
+                    </section>   
+                 </div>     
+        </div>
+</div>
     <div id="ScheduledTask">
         <div id="contentScheduledTask">
             <h1 id="titleScheduledTask">Scheduled Tasks</h1>
@@ -119,9 +114,8 @@ include 'components/header-nav/header-nav.php';
             </table>
         </div>
     </div>
-  </div>
-
 </div>
+
 <?php
 include 'components/modals/contact/contact.php';
 include 'components/footer/footer.php';
