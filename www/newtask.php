7de2058fc52d7fcc263ea83a85ff22cbc1b38607
alex <alex.de-coat@orange.fr>
2018-12-18 11:49:46 +0100

    the new, better, more advanced, newtask.php

--- www/newtask.php
+++ www/newtask.php
@@ -20,26 +20,40 @@ $dbname = 'mff';
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
+} 
 
 $sql = "SELECT * FROM presets WHERE id_home = $SESSION_home_id";
 $result = mysqli_query($conn, $sql);
 
 $yourpresets = array();
 
-if (mysqli_num_rows($result) > 0)
+if (mysqli_num_rows($result) > 0) 
 {
   // output data of each row
-  while($presets = mysqli_fetch_assoc($result))
+  while($presets = mysqli_fetch_assoc($result)) 
   {
       array_push($yourpresets, $presets);
   }
-}
+} 
 else {
   echo "You have no preset";
 }
 
 //les presets sont contenus dans la variable array $yourpresets//
+
+//maintenant : une fois le preset et les paramètres sélectionnés, les envoyer vers la bdd
+
+if (isset($_POST['savetask']))
+{
+  $selectedPreset = $_POST['preset'];
+  $selectedFrequency = $_POST['frequency'];
+  $selectedStartDate = $_POST['trip-start'];
+  $selectedHour = $_POST['time'];
+
+  $stmt = $conn->prepare("INSERT INTO tasks (name, deadline, frequency) VALUES (?, ?, ?)");
+  $stmt->bind_param("sss", $selectedPreset, $selectedStartDate, $selectedFrequency);
+  $stmt->execute();
+}
 
 ?>
 
@@ -73,7 +87,7 @@ else {
 
 if ($_SESSION['connected']){
 }
-include 'components/header-nav/header-nav.php';//
+//include 'components/header-nav/header-nav.php';//
 
 ?>
 
@@ -84,10 +98,12 @@ include 'components/header-nav/header-nav.php';//
             <div class="dashboard-inner-container">
 
               <div class="half-wrapper">
+              <form method="post">
                 <section class="selectors">
                   <span class="label">Presets</span>
                   <select class="dropdown" name="preset">
-                    <?php
+                  <option value ="notanoption" disabled selected>-- select preset --</option>
+                    <?php 
                     foreach ($yourpresets as $p){
                       echo "<option value =" . $p['id']. ">". $p['name'] . "</option>";
                     }
@@ -103,7 +119,8 @@ include 'components/header-nav/header-nav.php';//
                             <option value="Weekly">Weekly</option>
                             <option value="Monthly">Monthly</option>
                             <option value="One-time instance">One-time instance</option>
-                         </select>
+                  </select>
+
                 </section>
                 <section class="selectors">
                   <span class="label">Date</span>
@@ -111,10 +128,14 @@ include 'components/header-nav/header-nav.php';//
                 </section>
                 <section class="selectors">
                     <span class="label">Hour</span>
-                    <input type="time" name="chosen_time">
+                    <input type="time" name="time">
                 </section>
+                <br/><br/>
+                <center>
+                <input id="buttonSaveTask" type="submit" value="Save the task" name="savetask">
+                </center>
+                </form>
               </div>
-              <input id="buttonSaveTask" type="button" value="Save the task">
             </div>
           </div>
 
