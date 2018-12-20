b367dc3f2c3a454c152432a20a7feba2abc21907
Julie Adalian <julie.adalian3@gmail.com>
2018-12-20 17:56:32 +0100

    possibilité de changer le nom de la room

--- www/handlers/ajax-rooms-of-house.php
+++ www/handlers/ajax-rooms-of-house.php
@@ -5,6 +5,10 @@ $db = mysqli_connect('localhost', 'root', '', 'mff');
 mysqli_set_charset($db,"utf8");
 
 $homeId = $_SESSION['home_id'];
+
+
 $result = mysqli_query($db, "SELECT id, name FROM rooms WHERE id_home = $homeId");
 echo(mysqli_error($db));
 echo(json_encode(mysqli_fetch_all($result,MYSQLI_NUM)));
+
+?>
