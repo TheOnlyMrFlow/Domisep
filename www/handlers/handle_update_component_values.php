9d8f8b6b216f5364bb21fe7d920145d3ca63c45b
PlayZboy <matthieulormeau@gmail.com>
2019-01-11 21:32:11 +0100

    les composants sont fonctionnels

--- www/handlers/handle_update_component_values.php
+++ www/handlers/handle_update_component_values.php
@@ -8,14 +8,15 @@ $actionType = $_POST['action'];
 if ($actionType == 'change_state') {
   $state = $_POST['state'];
   $id = $_POST['id'];
-  if ($state =='false') {
-    $state = 1;
+  if ($state == 'false') {
+    $bool = 1;
+    echo($state);
   }
   elseif($state == 'true'){
-    $state = 0;
+    $bool = 0;
+    echo($state);
   }
-
-  $result = mysqli_query($db, "UPDATE components SET state=$state WHERE serial_number='$id'");
+  $result = mysqli_query($db, "UPDATE components SET state=$bool WHERE serial_number='$id'");
 }
 elseif ($actionType == 'add_value') {
   $id = $_POST['id'];
